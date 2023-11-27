7b59dbbebe43092aa557f6734b341db748e84a32
TIGER <suraj23082002@gmail.com>
2023-11-27 14:17:25 +0530

    fixed http headers error by simplifying the code

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const ejsMate = require("ejs-mate");
-const catchAsync = require("./utils/catchAsync");
-const ExpressError = require("./utils/ExpressError");
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
@@ -20,29 +18,21 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
-app.post("/", catchAsync((req, res) => {
-    const { range1, range2 } = req.body;
-    res.redirect(`/random?range1=${range1}&range2=${range2}`);
-}));
-
-app.get("/random", catchAsync((req, res) => {
+app.get("/random", (req, res) => {
+    try {
     const range1 = req.query.range1 > 0 ? req.query.range1 : 0;
     const range2 = req.query.range2 >= 1 ? req.query.range2 : 10;
-    res.render("random", { range1, range2 });
-}));
-
-app.all("*", (req, res, next) => {
-    next(new ExpressError("Page Not Found!", 404));
+    const range = [range1, range2];
+    res.render("random", { range });
+    }
+    catch (e) {
+        console.log(e);
+        res.render("error");
+    }
 });
 
-app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
-    if (!err.message) {
-        err.message = "Oh No, Something went wrong!";
-    }
-    else {
-        res.status(statusCode).render("error", { err });
-    };
+app.all("*", (req, res, next) => {
+    res.render("error");
 });
 
 const port = process.env.PORT || 3000;
